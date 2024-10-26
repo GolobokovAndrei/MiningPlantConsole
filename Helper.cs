31b8fc4b2d50d1db8f294310fa3447de09be5b79
fasterplaymc <agolobokov1@gmail.com>
2024-10-26 19:13:37 +0700

    Add project files.

---
+++ Helper.cs
@@ -0,0 +1,27 @@
+﻿using MiningPlantConsole.Model;
+using System;
+
+namespace MiningPlantConsole
+{
+    internal class Helper
+    {
+        private static MiningPlantEntities1 _context;
+
+        public static MiningPlantEntities1 GetContext()
+        {
+            if (_context == null)
+            {
+                _context = new MiningPlantEntities1();
+            }
+            return _context;
+        }
+
+        public void CreateUser(Users user)
+        {
+            var context = GetContext();
+
+            context.Users.Add(user);
+            context.SaveChanges();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using MiningPlantConsole;
+using Password;
+using MiningPlantConsole.Model;
+
+namespace MiningPlantConsole
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Prompt the user for input
+            Console.WriteLine("Создание новой учетной записи для пользователя");
+            Console.Write("Введите имя пользователя (логин): ");
+            string username = Console.ReadLine();
+
+            Console.Write("Введите пароль пользователя: ");
+            string password = Console.ReadLine();
+
+            // Hash the password
+            HashPassword hashPassword = new HashPassword();
+            string hashedPassword = hashPassword.GetHashedPassword(password);
+            Console.WriteLine($"Хешированный пароль: {hashedPassword}");
+
+            Users newUser = new Users
+            {
+                UserLogin = username,
+                UserPassword = hashedPassword,
+            };
+
+            Helper helper = new Helper();
+            try
+            {
+                helper.CreateUser(newUser);
+                Console.WriteLine("Учетная запись добавлена");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Ошибка при добавлении учетной записи: {ex.Message}");
+            }
+
+            Console.WriteLine("Нажмите любую клавишу для выхода...");
+            Console.ReadKey();
+        }
+    }
+}
